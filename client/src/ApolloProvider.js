0266511087abed78b78aee769420b2d8f94a0bcb
andrey-dum <duminskiy@gmail.com>
2021-02-18 20:11:47 +0200

    Display posts. PostCard

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,29 +1,5 @@
 import React from 'react';
 import App from './App';
-// import ApolloClient from 'apollo-client';
-// import { InMemoryCache } from 'apollo-cache-inmemory';
-// import { createHttpLink } from 'apollo-link-http';
-// import { ApolloProvider } from '@apollo/react-hooks';
-// import { setContext } from 'apollo-link-context';
-
-// const httpLink = createHttpLink({
-//   uri: 'http://localhost:5000'
-// });
-
-// const authLink = setContext(() => {
-//   const token = localStorage.getItem('jwtToken');
-//   return {
-//     headers: {
-//       Authorization: token ? `Bearer ${token}` : ''
-//     }
-//   };
-// });
-
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache()
-// });
-
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const cache = new InMemoryCache();
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client';
+import { Grid } from 'semantic-ui-react';
+import PostCard from '../components/PostCard';
+
+const FETCH_POST_QUERY = gql`
+    {
+        getPosts{
+            id 
+            body 
+            createdAt 
+            username 
+            likeCount
+            likes{
+                username
+            }
+            commentCount
+            comments{
+                id 
+                username 
+                createdAt 
+                body
+            }
+        }
+    }
+`
+
 
 export default function Home() {
+    const { 
+        loading, 
+        data, 
+        // data: { getPosts: posts }, 
+        error 
+    } = useQuery(FETCH_POST_QUERY) 
+
+    // if (loading) return <p>Loading...</p>;
+    // if (error) return <p>Error :(</p>;
+
     return (
-        <div>
-            Home
-        </div>
+        <Grid columns={3} divided>
+            <Grid.Row>
+               <h1 className="page__title">Recent Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+               { loading ? (
+                   <h1>Loading...</h1>
+               ) : (
+                data?.getPosts.map(post => (
+                    <Grid.Column key={post.id} style={{marginBottom: 25}}>
+                        <PostCard post={post} />
+                    </Grid.Column>
+                ))
+               ) }
+            </Grid.Row>
+
+        </Grid>
     )
 }
+
+

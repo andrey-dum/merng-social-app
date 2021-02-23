4379c949aae655ab5b475281afc899ec0fe64fcd
andrey-dum <duminskiy@gmail.com>
2021-02-23 20:52:04 +0200

    SinglePost. Comments

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import './App.css';
 
 import {AuthProvider} from './context/auth'
 import AuthRoute from './util/AuthRoute';
+import SinglePost from './pages/SinglePost';
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
           <Route exact path="/register" component={Register} /> */}
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
         </Container>
       </Router>
     </AuthProvider>
